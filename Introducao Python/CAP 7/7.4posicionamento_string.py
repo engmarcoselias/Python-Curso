7aa79233bf9b333e9ce6c57f140173193c9f0752
Marcos Vinicius Silva Elias <110794069+engmarcoselias@users.noreply.github.com>
2023-01-20 18:56:22 -0300

    Update 7.4posicionamento_string.py

--- Introducao Python/CAP 7/7.4posicionamento_string.py
+++ Introducao Python/CAP 7/7.4posicionamento_string.py
@@ -1,4 +1,28 @@
-#centralizar texto tamanho mais caracter de preenchimento
+#7.14centralizar texto (tamanho mais caracter de preenchimento)
 
 s = "Tigre"
-print("x"+s.center(10)+"x")
+print("x"+s.center(10,".")+"x")
+
+#7.15completar a string com espaço a direita e a esquerda
+
+#esquerda
+a = s.ljust(20)
+print(a)
+
+#direita
+b = s.rjust(40)
+print(b)
+
+#7.15completar a string com espaço a direita e a esquerda
+
+#esquerda
+a = s.ljust(20,".")
+print(a)
+
+#direita
+b = s.rjust(20,".")
+print(b)
+
+#substituição de string
+
+
