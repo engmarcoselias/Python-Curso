80e0c04d40430de24649b3584c3760f1ac5f3f84
Marcos Vinicius Silva Elias <eng.marcos.elias@gmail.com>
2023-02-08 21:40:19 -0300

    CAP 10
    -Método de comparação de uma classe
    -Functools

---
+++ Introducao Python/CAP 10/anotacoes.py
@@ -0,0 +1,26 @@
+#10.15usando anotações 
+
+from functools import total_ordering
+
+
+@total_ordering
+class Nome:
+    def __init__(self, nome):
+        if nome == None or not nome.strip():
+            raise ValueError("Nome não pode ser nulo nem em branco")
+        self.nome = nome
+        self.chave = Nome.CriaChave(nome)
+    def __str__(self):
+        return self.nome
+    def __repr__(self):
+        return "<Classe{3} en 0x{0:x} Nome: {1} Chave: {2}>" .format (id(self),self.nome, self.chave, type(self).__name__)
+    def __eq__(self, outro):
+        print("__eq__Chamado")
+        return self.nome == outro.nome
+    def __lt__(self,outro):
+        print("__lt__Chamado")
+        return self.nome < outro.nome
+    @staticmethod
+    def CriaChave(nome):
+        return nome.strip().lower()
+
