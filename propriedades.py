65c7f7dde3c8b43154e1f641892e1b3c346036ab
Marcos Vinicius Silva Elias <eng.marcos.elias@gmail.com>
2023-02-09 22:54:17 -0300

    CAP 10
    -Decoradores
    -Propriedades

---
+++ propriedades.py
@@ -0,0 +1,29 @@
+#10.16classe nome com propriesades
+
+from functools import total_ordering
+
+@total_ordering
+
+class Nome:
+    def __init__(self, nome):
+        self.nome = nome
+    def __str__(self):
+        return self.nome
+    def __repr__(self):
+        return "<Classe {3} em 0x{0:x}Nome: {1} Chave: {2}>" .format(id(self),self.__nome, self.__chave,type(self).__name__)
+    def __eq__(self, outro):
+        return self.nome == outro.nome
+    def __lt__(self, outro):
+         return self.nome < outro.nome
+    @property  #tranforma o metodo abaixo em propriedade(sempre que escrevermos objeto.nome do objeto chamamos esse metodo)
+    def nome(self):
+        return self.__nome        
+    @nome.setter  #tranforma o metodo abaixo em propriedade(sempre que escrevermos objeto.nome = um valor do objeto chamamos esse metodo)
+    def nome(self, valor):
+        if valor == None or  not valor.strip():
+            raise ValueError("Nome não pode se nulo nem em branco")
+        self.__nome = valor
+        self.__chave = Nome.CriaChave(valor)
+    @staticmethod
+    def CriaChave(nome):
+        return nome.strip().lower()
