8f256288805aa1e5bb94c86546ed31c05f9d184d
Marcos Vinicius Silva Elias <110794069+engmarcoselias@users.noreply.github.com>
2023-02-14 18:33:35 -0300

    Atualização CAP 11

--- Introducao Python/CAP 11/11.3consulta_SQL.py
+++ Introducao Python/CAP 11/11.3consulta_SQL.py
@@ -3,8 +3,8 @@
 import sqlite3
 conexão = sqlite3.connect("agenda.db")
 cursor = conexão.cursor()
-cursor.execute("select * from agenda")
-resultado = cursor.fetchone()
+cursor.execute("select * from agenda")#comando select realiza consulta o asterisco representa todos os campos da tabela sendo consultada(nese caso nome e telefone)
+resultado = cursor.fetchone()#o methodo fetchone e utilizado para acessar e o resultado do cursor e retorna uma tupla ou None caso a tabela esteja vazia
 print("Nome: %s\nTelefone: %s"% (resultado))
 cursor.close
 conexão.close
