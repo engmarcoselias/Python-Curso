4d39aef988f307ade96cc34a75746b6c518ce938
Marcos Vinicius Silva Elias <110794069+engmarcoselias@users.noreply.github.com>
2023-02-15 18:48:16 -0300

    Atualização
    Atualização

--- Introducao Python/CAP 11/11.4inserindo.py
+++ Introducao Python/CAP 11/11.4inserindo.py
@@ -1,1 +1,11 @@
 #11.3inserindo multiplos registros
+
+import sqlite3
+
+dados = [("joão", "92242588"),("andre", "995656565"),("Marcos","96591999991")]
+conexão = sqlite3.connect("agenda.db")
+cursor = conexão.cursor()
+cursor.executemany('''insert into agenda(nome, telefone)values(?,?)''', dados)
+conexão.commit()
+cursor.close()
+conexão.close()
