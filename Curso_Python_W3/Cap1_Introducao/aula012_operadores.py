7b980a924336629570e8fc5bfc63660a35e16b90
Marcos Vinicius Silva Elias <eng.marcos.elias@gmail.com>
2024-07-16 11:38:01 -0300

    Update aula012_operadores.py

--- Curso_Python_W3/Cap1_Introducao/aula012_operadores.py
+++ Curso_Python_W3/Cap1_Introducao/aula012_operadores.py
@@ -57,3 +57,62 @@ Operadores bit a bit
 # <<=	x <<= 3	x = x << 3	
 # :=	print(x := 3) x = 3
 
+#Operadores de comparação Python
+
+#Operadores de comparação são usados ​​para comparar dois valores:
+
+
+# ==	Equal	x == y	
+# !=	Not equal	x != y	
+# >	Greater than	x > y	
+# <	Less than	x < y	
+# >=	Greater than or equal to	x >= y	
+# <=	Less than or equal to	x <= y
+
+
+# Operadores Lógicos Python
+
+#Operadores lógicos são usados ​​para combinar instruções condicionais:
+
+
+# and 	Returns True if both statements are true	x < 5 and  x < 10
+	
+# or	Returns True if one of the statements is true	x < 5 or x < 4	
+
+#not	Reverse the result, returns False if the result is true	not(x < 5 and x < 10)
+
+
+#Operadores de identidade Python
+
+
+#Operadores de identidade são usados ​​para comparar os objetos, não se eles são iguais, mas se eles são realmente o mesmo objeto, com o mesmo local de memória:
+
+
+#is 	Returns True if both variables are the same object	x is y
+
+#is not	Returns True if both variables are not the same object	x is not y
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y)
+
+
+#Operadores de Associação Python
+
+#Operadores de associação são usados ​​para testar se uma sequência é apresentada em um objeto:
+
+
+#in 	Returns True if a sequence with the specified value is present in the object	x in y
+	
+#not in	Returns True if a sequence with the specified value is not present in the object	x not in y
+
+x = ["apple", "banana"]
+
+print("banana" in x)
+
+
+
